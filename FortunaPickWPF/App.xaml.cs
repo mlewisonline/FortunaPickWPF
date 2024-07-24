7ed06331c0fd8b9cb8c6f80186128743987524d0
Matthew Lewis <mlewis1981@outlook.com>
2024-07-24 09:24:28 +0100

    Project Base template setup, MVVM Folders setup, DI setup, Material Design setup.

--- FortunaPickWPF/App.xaml.cs
+++ FortunaPickWPF/App.xaml.cs
@@ -1,14 +1,35 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
-namespace FortunaPickWPF
+namespace FortunaPickWPF;
+
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    [STAThread]
+    public static void Main(string[] args)
     {
+        using IHost host = CreateHostBuilder(args).Build();
+        host.Start();
+
+        App app = new();
+        app.InitializeComponent();
+        app.MainWindow = host.Services.GetRequiredService<MainWindow>();
+        app.MainWindow.Visibility = Visibility.Visible;
+        app.Run();
     }
 
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration((hostBuilderContext, configurationBuilder)
+            => configurationBuilder.AddUserSecrets(typeof(App).Assembly))
+        .ConfigureServices((hostContext, services) =>
+        {
+            services.AddSingleton<MainWindow>();
+ 
+        });
 }
